e247d6ec436b3c3f224999bde0987a69e8e66241
FernandaRojas29 <monica_frojas@soy.sena.edu.co>
2025-06-14 16:19:43 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { CarritoComponent } from './pages/carrito/carrito.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'productos', component: ProductosComponent },
+  { path: 'carrito', component: CarritoComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'contacto', component: ContactoComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
