f084252c044691b9a198dfc8d3d1f209747df9dd
FernandaRojas29 <monica_frojas@soy.sena.edu.co>
2025-06-17 17:06:24 -0500

    se realizo la pagina de inicio de sesion, carrito y perfil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { CheckoutComponent } from './pages/checkout/checkout.component';
 import { ContactoComponent } from './pages/contacto/contacto.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RegistroComponent } from './pages/registro/registro.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -14,6 +16,9 @@ export const routes: Routes = [
   { path: 'checkout', component: CheckoutComponent },
   { path: 'contacto', component: ContactoComponent },
   { path: 'registro', component: RegistroComponent },
+  { path: "perfil", component: PerfilComponent},
+  { path: "inicio", component: InicioComponent},
+
 
 ];
 
--- src/app/pages/carrito/carrito.component.ts
+++ src/app/pages/carrito/carrito.component.ts
@@ -1,12 +1,68 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-carrito',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf, DatePipe],
   templateUrl: './carrito.component.html',
   styleUrl: './carrito.component.css'
 })
 export class CarritoComponent {
+  carrito = [
+    {
+      nombre: 'Producto Ejemplo',
+      precio: 20,
+      cantidad: 1,
+      imagen: 'https://via.placeholder.com/100'
+    },
+    {
+      nombre: 'Otro Producto',
+      precio: 45,
+      cantidad: 2,
+      imagen: 'https://via.placeholder.com/100'
+    }
+  ];
 
+  facturaGenerada = false;
+  facturaResumen: any = null;
+
+  aumentar(i: number) {
+    this.carrito[i].cantidad++;
+  }
+
+  disminuir(i: number) {
+    if (this.carrito[i].cantidad > 1) {
+      this.carrito[i].cantidad--;
+    }
+  }
+
+  eliminar(i: number) {
+    this.carrito.splice(i, 1);
+  }
+
+  obtenerSubtotal(): number {
+    return this.carrito.reduce((total, p) => total + p.precio * p.cantidad, 0);
+  }
+
+  comprar() {
+    this.facturaResumen = {
+      cliente: 'Juanito Rodríguez',
+      productos: this.carrito.map(p => ({
+        nombre: p.nombre,
+        cantidad: p.cantidad,
+        precio: p.precio,
+        total: p.precio * p.cantidad
+      })),
+      total: this.obtenerSubtotal(),
+      fecha: new Date()
+    };
+
+    this.facturaGenerada = true;
+    this.carrito = [];
+  }
+
+  volverAlCarrito() {
+    this.facturaGenerada = false;
+  }
 }
